8f05c9abe5a7c277b1ba5e1d2ee23fdbf8df35e6
dev_woo <mskim2525@naver.com>
2025-01-22 15:51:56 +0900

    Add NumberInput 컴퍼넌트 구현

--- src/component/NumberInput.jsx
+++ src/component/NumberInput.jsx
@@ -1,8 +1,4 @@
 function NumberInput({ label, value, onChange }) {
-	function handleChange(e) {
-		const value = Number(e.target.value);
-	}
-
 	return (
 		<label>
 			{label}
@@ -10,6 +6,7 @@ function NumberInput({ label, value, onChange }) {
 				type="number"
 				value={value}
 				onChange={(e) => onChange(Number(e.target.value))}
+				min={0}
 			/>
 		</label>
 	);
